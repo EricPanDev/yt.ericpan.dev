0fc3a3cc76224127a0b48aa8ea94518844734b0f
EricPanDev <darkknightmaster666@gmail.com>
2024-10-10 12:56:25 -0700

    push

---
+++ downloader.py
@@ -0,0 +1,41 @@
+import yt_dlp
+
+def get_download_url(video_url):
+    if not "youtube.com" in video_url:
+        video_url = "https://www.youtube.com/watch?v=" + video_url
+
+    ydl_opts = {
+        'format': 'best',
+        'noplaylist': True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(video_url, download=False)
+
+        best_format = next((f for f in info_dict['formats'] if f['format_id'] == info_dict['format_id']), None)
+
+        if best_format:
+            download_url = best_format['url']
+            return download_url
+        else:
+            raise NotImplementedError("No suitable format found for download")
+        
+        
+def get_video_thumbnail(video_url):
+    if not "youtube.com" in video_url:
+        video_url = "https://www.youtube.com/watch?v=" + video_url
+
+    ydl_opts = {
+        'format': 'best',
+        'noplaylist': True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(video_url, download=False)
+        
+        thumbnail_url = info_dict.get('thumbnail')
+        
+        if thumbnail_url:
+            return thumbnail_url
+        else:
+            raise NotImplementedError("No thumbnail found for the video")
---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, redirect, request, make_response, render_template
+import requests
+
+import downloader
+app = Flask(__name__)
+
+@app.route('/download/<url>')
+def download(url):
+    file_url = downloader.get_download_url(url)
+    
+    response = make_response(redirect(file_url))
+    
+    response.headers['Content-Disposition'] = 'attachment; filename="file.zip"'
+    
+    return response
+
+@app.route('/preview/<url>')
+def preview(url):
+    if "=" in url:
+        url = url[::-1].split("=",1)[0][::-1]
+    
+    image_url = f"https://i.ytimg.com/vi_webp/{url}/maxresdefault.webp"
+    
+    # Fetch the image
+    response = requests.get(image_url)
+
+    # Create a response with the image data
+    response_to_client = make_response(response.content)
+    response_to_client.headers['Content-Type'] = response.headers['Content-Type']
+    response_to_client.headers['Access-Control-Allow-Origin'] = '*'
+
+    return response_to_client
+        
+    thumbnail_url = downloader.get_video_thumbnail(url)
+    
+    return redirect(thumbnail_url)
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
